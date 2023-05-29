146c18750b16e773015444a593151cb37ece8169
unknown <farzad.edsoft@gmail.com>
2023-05-30 02:22:36 +0600

    Data filter portion done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,97 +6,143 @@ use App\Models\Product;
 use App\Models\Variant;
 use App\Models\ProductVariant;
 use Illuminate\Http\Request;
+use App\Http\Requests\ProductFilterRequest;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
-     */
-    public function index()
-    {
-    
-            $products = Product::with(['variantPrices'])->paginate(5);
-            $product_variants = ProductVariant::all();
+/**
+ * Display a listing of the resource.
+ *
+ * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+ */
+public function index(ProductFilterRequest $request)
+{
+    //  dd($request->all());
+     // For search or filter Product..........
+     $query = Product::query();
 
-            // dd($products);
+     if ($request->filled('title')) {
+         $query->where('title', 'like', '%' . $request->input('title') . '%');
+     }
+ 
+     if ($request->filled('variant')) {
+         $query->whereHas('variantPrices', function ($subQuery) use ($request) {
+             $subQuery->whereIn('product_variant_one', [$request->input('variant')])
+                 ->orWhereIn('product_variant_two', [$request->input('variant')])
+                 ->orWhereIn('product_variant_three', [$request->input('variant')]);
+         });
+     }
+ 
+     if ($request->filled('price_from') && $request->filled('price_to')) {
+         $query->whereHas('variantPrices', function ($subQuery) use ($request) {
+             $subQuery->whereBetween('price', [$request->input('price_from'), $request->input('price_to')]);
+         });
+     }
+ 
+     if ($request->filled('date')) {
+         $query->whereDate('created_at', $request->input('date'));
+     }
+ 
+     $filteredProducts = $query->paginate(5);
+   
+        //  dd($filteredProducts);
 
-            return view('products.index',
-                compact(
-                    'products',
-                    'product_variants'
-                   
-                ));
+    // For list of the Tables.....
 
-    }
+        $products = Product::with(['variantPrices'])->paginate(5);
+        $product_variants = Variant::pluck('title');
+        // dd($product_variants);
+        $variantOneValues = ProductVariant::distinct('variant')->pluck('variant');
+        // dd($variantOneValues);
+        $variantTwoValues = ProductVariant::distinct('variant')->pluck('variant');
+        // dd($variantTwoValues);
+        $variantThreeValues = ProductVariant::distinct('variant')->pluck('variant');
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
-     */
-    public function create()
-    {
-        $variants = Variant::all();
-        return view('products.create', compact('variants'));
-    }
+        $data = [
+            'Color' => $variantOneValues->slice(0, 3),
+            'Size' => $variantOneValues->slice(3, 4),
+            'Style' => $variantOneValues->slice(7),
+        ];
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function store(Request $request)
-    {
+        // dd($products);
 
-    }
+        return view('products.index',
+            compact(
+                'products',
+                'product_variants',
+                'filteredProducts',
+                'data'
+                
+            ));
+
+}
+
+/**
+ * Show the form for creating a new resource.
+ *
+ * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+ */
+public function create()
+{
+    $variants = Variant::all();
+    return view('products.create', compact('variants'));
+}
+
+/**
+ * Store a newly created resource in storage.
+ *
+ * @param \Illuminate\Http\Request $request
+ * @return \Illuminate\Http\JsonResponse
+ */
+public function store(Request $request)
+{
+
+}
 
 
-    /**
-     * Display the specified resource.
-     *
-     * @param \App\Models\Product $product
-     * @return \Illuminate\Http\Response
-     */
-    public function show($product)
-    {
+/**
+ * Display the specified resource.
+ *
+ * @param \App\Models\Product $product
+ * @return \Illuminate\Http\Response
+ */
+public function show($product)
+{
 
-    }
+}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param \App\Models\Product $product
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Product $product)
-    {
-        $variants = Variant::all();
-        return view('products.edit', compact('variants'));
-    }
+/**
+ * Show the form for editing the specified resource.
+ *
+ * @param \App\Models\Product $product
+ * @return \Illuminate\Http\Response
+ */
+public function edit(Product $product)
+{
+    $variants = Variant::all();
+    return view('products.edit', compact('variants'));
+}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param \App\Models\Product $product
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Product $product)
-    {
-        //
-    }
+/**
+ * Update the specified resource in storage.
+ *
+ * @param \Illuminate\Http\Request $request
+ * @param \App\Models\Product $product
+ * @return \Illuminate\Http\Response
+ */
+public function update(Request $request, Product $product)
+{
+    //
+}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param \App\Models\Product $product
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Product $product)
-    {
-        //
-    }
+/**
+ * Remove the specified resource from storage.
+ *
+ * @param \App\Models\Product $product
+ * @return \Illuminate\Http\Response
+ */
+public function destroy(Product $product)
+{
+    //
+}
 }
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,112 +4,168 @@
 @php
 use App\Models\ProductVariant;
 @endphp
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Products</h1>
-    </div>
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">Products</h1>
+</div>
 
 
-    <div class="card">
-        <form action="" method="get" class="card-header">
-            <div class="form-row justify-content-between">
-                <div class="col-md-2">
-                    <input type="text" name="title" placeholder="Product Title" class="form-control">
-                </div>
-                <div class="col-md-2">
-                   <select name="variant" id="" class="form-control">
-                        <option value="" disabled selected>--Select variant--</option>
-                        @foreach($product_variants as $item)
-                            <option value="{{$item->id}}">{{$item->variant}} </option>
-                        @endforeach
-                    </select>
-                </div>
-
-                <div class="col-md-3">
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text">Price Range</span>
-                        </div>
-                        <input type="text" name="price_from" aria-label="First name" placeholder="From" class="form-control">
-                        <input type="text" name="price_to" aria-label="Last name" placeholder="To" class="form-control">
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <input type="date" name="date" placeholder="Date" class="form-control">
-                </div>
-                <div class="col-md-1">
-                    <button type="submit" class="btn btn-primary float-right"><i class="fa fa-search"></i></button>
-                </div>
+<div class="card">
+    <form action="{{ route('product.index') }}" method="get" class="card-header">
+        <div class="form-row justify-content-between">
+            <div class="col-md-2">
+                <input type="text" name="title" placeholder="Product Title" class="form-control">
             </div>
-        </form>
-
-        <div class="card-body">
-            <div class="table-response">
-                <table class="table">
-                    <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Title</th>
-                        <th>Description</th>
-                        <th>Variant</th>
-                        <th width="150px">Action</th>
-                    </tr>
-                    </thead>
-
-                    <tbody>
-        @foreach ($products as $product)
-            <tr>
-                <td>{{ $product->id }}</td>
-                <td>{{ $product->title }}<br>Created at: {{\Carbon\Carbon::parse($product->created_at)->format('d-M-Y') }}</td>
-                <td>{!! Str::limit($product->description, 50) !!}</td>
-                <td>
-                    <dl class="row mb-0" style="height: 80px; overflow: hidden" id="variant">
-                        @foreach ($product->variantPrices->groupBy('product_variant_one', 'product_variant_two', 'product_variant_three') as $prices)
-                            @foreach ($prices as $price)
-                            @php
-                                $variantNames = ProductVariant::whereIn('id', [
-                                    $price->product_variant_one,
-                                    $price->product_variant_two,
-                                    $price->product_variant_three
-                                ])->pluck('variant')->implode(' / ');
-                            @endphp
-                                <dt class="col-sm-3 pb-0">
-                                    {{ $variantNames }}
-                                </dt>
-                                <dd class="col-sm-9">
-                                    <dl class="row mb-0">
-                                        <dt class="col-sm-4 pb-0">Price: {{ number_format($price->price, 2) }}</dt>
-                                        <dd class="col-sm-8 pb-0">InStock: {{ number_format($price->stock, 2) }}</dd>
-                                    </dl>
-                                </dd>
+            <div class="col-md-2">
+                        <select name="variant" id="" class="form-control">
+                            <option value="" disabled selected>--Select variant--</option>
+                            @foreach ($data as $title => $values)
+                                <optgroup label="{{ $title }}">
+                                    @foreach ($values as $value)
+                                        <option value="{{ $value }}">{{ $value }}</option>
+                                    @endforeach
+                                </optgroup>
                             @endforeach
-                        @endforeach
-                    </dl>
-                    <button onclick="$('#variant').toggleClass('h-auto')" class="btn btn-sm btn-link">Show more</button>
-                </td>
-                <td>
-                    <div class="btn-group btn-group-sm">
-                        <a href="{{ route('product.edit', $product->id) }}" class="btn btn-success">Edit</a>
-                    </div>
-                </td>
-            </tr>
-        @endforeach
-        </tbody>
-
-                </table>
+                        </select>
             </div>
 
+            <div class="col-md-3">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">Price Range</span>
+                    </div>
+                    <input type="text" name="price_from" aria-label="First name" placeholder="From" class="form-control">
+                    <input type="text" name="price_to" aria-label="Last name" placeholder="To" class="form-control">
+                </div>
+            </div>
+            <div class="col-md-2">
+                <input type="date" name="date" placeholder="Date" class="form-control">
+            </div>
+            <div class="col-md-1">
+                <button type="submit" class="btn btn-primary float-right"><i class="fa fa-search"></i></button>
+            </div>
+        </div>
+    </form>
+
+    <div class="card-body">
+        <div class="table-response">
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Variant</th>
+                    <th width="150px">Action</th>
+                </tr>
+                </thead>
+
+                <tbody>
+                @if ($filteredProducts->isNotEmpty())
+
+            @foreach ($filteredProducts as $product)
+                
+                        <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $product->title }}<br>Created at: {{\Carbon\Carbon::parse($product->created_at)->format('d-M-Y') }}</td>
+                        <td>{!! Str::limit($product->description, 50) !!}</td>
+                        <td>
+                            <dl class="row mb-0" style="height: 80px; overflow: hidden" id="variant">
+                                @foreach ($product->variantPrices->groupBy('product_variant_one', 'product_variant_two', 'product_variant_three') as $prices)
+                                    @foreach ($prices as $price)
+                                    @php
+                                        $variantNames = ProductVariant::whereIn('id', [
+                                            $price->product_variant_one,
+                                            $price->product_variant_two,
+                                            $price->product_variant_three
+                                        ])->pluck('variant')->implode(' / ');
+                                    @endphp
+                                        <dt class="col-sm-3 pb-0">
+                                            {{ $variantNames }}
+                                        </dt>
+                                        <dd class="col-sm-9">
+                                            <dl class="row mb-0">
+                                                <dt class="col-sm-4 pb-0">Price: {{ number_format($price->price, 2) }}</dt>
+                                                <dd class="col-sm-8 pb-0">InStock: {{ number_format($price->stock, 2) }}</dd>
+                                            </dl>
+                                        </dd>
+                                    @endforeach
+                                @endforeach
+                            </dl>
+                            <button onclick="$('#variant').toggleClass('h-auto')" class="btn btn-sm btn-link">Show more</button>
+                        </td>
+                        <td>
+                            <div class="btn-group btn-group-sm">
+                                <a href="{{ route('product.edit', $product->id) }}" class="btn btn-success">Edit</a>
+                            </div>
+                        </td>
+                    </tr>
+             @endforeach
+
+                @else
+
+    @foreach ($products as $product)
+        <tr>
+            <td>{{ $loop->iteration }}</td>
+            <td>{{ $product->title }}<br>Created at: {{\Carbon\Carbon::parse($product->created_at)->format('d-M-Y') }}</td>
+            <td>{!! Str::limit($product->description, 50) !!}</td>
+            <td>
+                <dl class="row mb-0" style="height: 80px; overflow: hidden" id="variant">
+                    @foreach ($product->variantPrices->groupBy('product_variant_one', 'product_variant_two', 'product_variant_three') as $prices)
+                        @foreach ($prices as $price)
+                        @php
+                            $variantNames = ProductVariant::whereIn('id', [
+                                $price->product_variant_one,
+                                $price->product_variant_two,
+                                $price->product_variant_three
+                            ])->pluck('variant')->implode(' / ');
+                        @endphp
+                            <dt class="col-sm-3 pb-0">
+                                {{ $variantNames }}
+                            </dt>
+                            <dd class="col-sm-9">
+                                <dl class="row mb-0">
+                                    <dt class="col-sm-4 pb-0">Price: {{ number_format($price->price, 2) }}</dt>
+                                    <dd class="col-sm-8 pb-0">InStock: {{ number_format($price->stock, 2) }}</dd>
+                                </dl>
+                            </dd>
+                        @endforeach
+                    @endforeach
+                </dl>
+                <button onclick="$('#variant').toggleClass('h-auto')" class="btn btn-sm btn-link">Show more</button>
+            </td>
+            <td>
+                <div class="btn-group btn-group-sm">
+                    <a href="{{ route('product.edit', $product->id) }}" class="btn btn-success">Edit</a>
+                </div>
+            </td>
+        </tr>
+    @endforeach
+
+    @endif
+    </tbody>
+            </table>
         </div>
 
-        <div class="card-footer">
-                <div class="row justify-content-between">
-                    <div class="col-md-6">
-                        <p>Showing {{ $products->firstItem() }} to {{ $products->lastItem() }} out of {{$products->total()}}</p>
-                    </div>
-                    <div class="col-md-2">
-                        {{ $products->links() }}
-                    </div>
-                </div>
+    </div>
+
+    <div class="card-footer">
+        <div class="row justify-content-between">
+            <div class="col-md-6">
+                @if ($filteredProducts->isNotEmpty())
+                    <p>Showing {{ $filteredProducts->firstItem() }} to {{ $filteredProducts->lastItem() }} out of {{ $filteredProducts->total() }}</p>
+                @else
+                    <p>Showing {{ $products->firstItem() }} to {{ $products->lastItem() }} out of {{ $products->total() }}</p>
+                @endif
+            </div>
+            <div class="col-md-2">
+                @if ($filteredProducts->isNotEmpty())
+                    {{ $filteredProducts->links() }}
+                @else
+                    {{ $products->links() }}
+                @endif
+            </div>
         </div>
     </div>
+</div>
 
 @endsection
